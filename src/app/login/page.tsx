39e9491cd344ee75fba12564bb2c0f6bacd441b2
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2025-05-26 06:31:43 -0300

    💄 refactor : loginPage style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,71 +2,15 @@ import { login } from "./action";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-950 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 bg-gray-50 p-10 rounded-lg shadow-xl">
-        <div className="text-center">
-          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-            Acesse sua conta
-          </h2>
+    <div className="flex justify-center items-center w-full h-full">
+        <div className="bg-gray-100 w-[300px] h-[250px] flex justify-center items-center rounded">
+          <form action={login} className="flex flex-col gap-3">
+            <h1 className="text-gray-950">Acesse sua conta!</h1>
+            <input name="email" type="email" placeholder='Email' required className="bg-gray-50 rounded text-gray-950 p-2 w-full border" defaultValue="dionatan@supabase.next"/>
+            <input name="password" type="password" placeholder='Senha' required className="bg-gray-50 rounded text-gray-950 p-2 w-full border"/>
+            <button type="submit" className="bg-indigo-600 p-2 rounded cursor-pointer">Entrar</button>
+          </form>
         </div>
-        <form className="mt-8 space-y-6" action={login}>
-          <div className="rounded-md shadow-sm space-y-4">
-            <div>
-              <label htmlFor="email" className="sr-only">
-                Email
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email"
-                defaultValue="dionatan@supabase.next"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Senha
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Senha"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-colors duration-200 cursor-pointer"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-              Entrar
-            </button>
-          </div>
-        </form>
-      </div>
     </div>
-  );
+  )
 }
