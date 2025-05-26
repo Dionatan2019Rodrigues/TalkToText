91cc0d0673f1e4a46cac29968497e9c7c46eedf0
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2025-05-26 06:16:35 -0300

    💄 feat : dashboardPage style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,17 +6,34 @@ const DashboardPage = async () => {
   const supabase = await createClient();
   const { data: todos } = await supabase.from("todos").select("*");
   return (
-    <div>
-      <form action={createNote}>
-        <input type="text" name="task" placeholder="Digite sua anotação..." />
-        <button type="submit" className="bg-gray-50 cursor-pointer text-black">Criar</button>
-      </form>
-      {todos?.map((todo) => (
-        <div key={todo.id} className="flex ">
-            <h2>{todo.task}</h2>
-            <DeleteButton id={todo.id}/>
+    <div className="p-10">
+      <div className="flex flex-col gap-5">
+        <form action={createNote} className="flex gap-2">
+          <input
+            type="text"
+            name="task"
+            placeholder="Digite sua anotação..."
+            className="bg-gray-50 rounded text-gray-950 p-2 w-full"
+          />
+          <button
+            type="submit"
+            className="bg-indigo-600 text-gray-50 px-3 rounded cursor-pointer"
+          >
+            Criar
+          </button>
+        </form>
+        <div className="flex flex-col gap-3">
+          {todos?.map((todo) => (
+            <div
+              key={todo.id}
+              className="w-full flex justify-between items-center border rounded p-2"
+            >
+              <h2>{todo.task}</h2>
+              <DeleteButton id={todo.id} />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
